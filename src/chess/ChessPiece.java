047fea3583e473c44b8008fdd395cd12b76300fd
Gabriel <silvanovaesgabriel@gmail.com>
2021-05-05 19:29:11 -0300

    Handling captured pieces

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,7 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
-
+	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
